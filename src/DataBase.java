f975104a489975931e19e1b916de0331f80131e4
benbsaha <saharbenbaruch@gmail.com>
2019-11-18 18:33:54 +0200

    connect to db + fuction FileToDatabase().

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,6 +1,8 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
 
 public class DataBase {
     Connection conn;
@@ -10,24 +12,61 @@ public class DataBase {
     //static final String DB_URL="jdbc:oracle:thin:@localhost:1521:orcl";
     String DB_URL;
 
-    public DataBase(String connection, String db_username, String pass)  {
+    public DataBase(String connection, String db_username, String db_pass) {
         try {
             Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        username=db_username;
-        password=pass;
-        DB_URL=connection;
+        username = db_username;
+        password = db_pass;
+        DB_URL = connection;
         try {
-            conn= DriverManager.getConnection(DB_URL,username,password);
+            conn = DriverManager.getConnection(DB_URL, username, password);
+            conn.setAutoCommit(false);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         System.out.println("connecting to database..");
     }
 
-    public void fileToDataBase(String path){
+    public void fileToDataBase(String path) {
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
 
+        try {
+
+            try {
+                br = new BufferedReader(new FileReader(path));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            while (true) {
+                try {
+                    if (!((line = br.readLine()) != null)) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // use comma as separator
+                if (conn!= null) {
+                    String[] itemDetails = line.split(cvsSplitBy);
+                    String itemTitle = itemDetails[0];
+                    String itemYear = itemDetails[1];
+                    PreparedStatement stat = conn.prepareStatement("INSERT INTO MEDIAITEMS (TITLE,PROD_YEAR) VALUES (?,?)");
+                    stat.setString(1, itemTitle);
+                    stat.setString(2, itemYear);
+                    stat.executeUpdate();
+                    conn.commit();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void calculateSimilarity(){
+        
     }
 }
